9d861c6630f1b5c3c7a25dcc6decffed40b6f7fe
Romain1664 <roomain.s.31@gmail.com>
2020-05-25 16:34:50 +0200

    creation component inscription

--- NCDZ angular/src/app/app.module.ts
+++ NCDZ angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule
