688c2e57f3280fabc51e25834a8b70bde3e34155
AntoineVrinat <antoine.vrinat.91@gmail.com>
2020-05-25 14:36:58 +0200

    push revision class

--- NCDZ angular/src/app/revision-parachute.ts
+++ NCDZ angular/src/app/revision-parachute.ts
@@ -2,5 +2,5 @@ import { Parachutiste } from './parachutiste';
 
 export class RevisionParachute {
     public parachutiste: Parachutiste;
-    constructor(public id?: number){}
+    constructor(public id?: number, public date_dernier_pliage_primaire?: Date, public date_dernier_pliage_secours?: Date){}
 }
