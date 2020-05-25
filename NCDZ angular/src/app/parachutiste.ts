c3e223f51bd2c752e5e219f0e5c92f8f55726907
Xalbat_Itoiz <xalbat.itoiz@gmail.com>
2020-05-25 14:57:05 +0200

    Class angular

--- NCDZ angular/src/app/parachutiste.ts
+++ NCDZ angular/src/app/parachutiste.ts
@@ -2,5 +2,6 @@ import { Parachute } from './parachute';
 
 export class Parachutiste {
     public parachute: Parachute;
+    
     constructor(public numeroLicence?: number, public nom?: string, public prenom?: string){}
 }
