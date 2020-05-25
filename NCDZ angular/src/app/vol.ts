fdf7cf6ed15914838f2ec1f9867e670ed23171a3
Lalie <modestecoralie@orange.fr>
2020-05-25 15:53:08 +0200

    correction Vol

--- NCDZ angular/src/app/vol.ts
+++ NCDZ angular/src/app/vol.ts
@@ -1,9 +1,11 @@
 import { Pilote } from './pilote';
 import { Avion } from './avion';
+import { Parachutiste } from './parachutiste';
 
 export class Vol {
     public pilote: Pilote;
     public avion: Avion;
-
-    constructor(public id?: number,public situationVol?: string){}
+    public parachutiste : Parachutiste; 
+    
+    constructor(public id?: number,public situationVol?: string, public respoVol?:Parachutiste, public respoSol?:Parachutiste){}
 }
