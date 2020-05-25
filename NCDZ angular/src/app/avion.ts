1f69b0beb0dedeff57fdbd7a6e1203d6e4a3f8bc
Romain1664 <roomain.s.31@gmail.com>
2020-05-25 15:16:24 +0200

    reprise argument avion snake

--- NCDZ angular/src/app/avion.ts
+++ NCDZ angular/src/app/avion.ts
@@ -1,3 +1,3 @@
 export class Avion {
-    constructor(public AVION_ID?: number, public AVION_HAUTEUR?: number, public AVION_CAPACITE?: number, public AVION_ROTATIONMAX?: number, public AVION_ROTATION?: number, public AVION_TEMPSMONTEE?: number, public AVION_ETAT?: boolean, public AVION_SITUATION?: string){}
+    constructor(public id?: number, public hauteur?: number, public capacite?: number, public rotationMax?: number, public rotation?: number, public tempsMontee?: number, public etat?: boolean, public situation?: string){}
 }
