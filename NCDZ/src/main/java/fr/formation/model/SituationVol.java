2441917b4c7d390d61fd5f13fe6955758ced85a4
Lalie <modestecoralie@orange.fr>
2020-05-25 13:47:50 +0200

    +Getter

--- NCDZ/src/main/java/fr/formation/model/SituationVol.java
+++ NCDZ/src/main/java/fr/formation/model/SituationVol.java
@@ -1,5 +1,9 @@
 package fr.formation.model;
 
 public enum SituationVol {
-	EN_ATTENTE, EN_PREPARATION, EMBARQUEMENT, EN_VOL, ATTERRISSAGE, INCIDENT
+	EN_ATTENTE, EN_PREPARATION, EMBARQUEMENT, EN_VOL, ATTERRISSAGE, INCIDENT;
+	
+	public SituationVol getSituationVol() {
+		return null;
+	}
 }
