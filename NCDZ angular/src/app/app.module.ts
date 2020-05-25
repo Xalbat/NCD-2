c153a5a47df4c53d4e116d3138b4eb73071bed54
Thomas <monsieurgt@gmail.com>
2020-05-25 18:30:44 +0200

    Premices component composer-vol (petit bug chez moi)

--- NCDZ angular/src/app/app.module.ts
+++ NCDZ angular/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ComposerVolComponent } from './composer-vol/composer-vol.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent
+    NgModule,
+    InscriptionComponent,
+    ComposerVolComponent
   ],
   imports: [
     BrowserModule
---
+++ NCDZ angular/src/app/composer-vol/composer-vol.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Avion } from '../avion';
+import { Parachutiste } from '../parachutiste';
+import { Saut } from '../saut';
+
+@Component({
+  selector: 'composer-vol',
+  templateUrl: './composer-vol.component.html',
+  styleUrls: ['./composer-vol.component.css']
+})
+export class ComposerVolComponent implements OnInit {
+
+  avion : Avion = new Avion()
+  avionsDisponibles : Array<Avion> = []
+  listeAttente : Array<Parachutiste> = []
+  listeAttenteGroupe : Array<Array<Parachutiste>> = []
+  listeParaConfirmés : Array<Parachutiste> = []
+  listeSautDemandés : Array<Saut> = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadListeAvionsDispo()
+    this.loadListesPara()
+  }
+
+  loadListeAvionsDispo(){
+    this.avionsDisponibles.push(new Avion(0,2500,4,3,0,60))
+    this.avionsDisponibles.push(new Avion(0,4000,3,3,0,60))
+  }
+
+  loadListesPara(){
+    this.listeAttente.push(new Parachutiste(2000,"Dupont","Jean",new Date('2020-10-05')))
+    this.listeSautDemandés.push(new Saut(0,2500,true))
+  }
+
+}
