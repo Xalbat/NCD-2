a8a91ea9156809935741c6536e69dfd9a4c3d79a
Thibault <dudouit.thibault@outlook.com>
2020-05-25 17:07:12 +0200

    Composant

--- NCDZ angular/src/app/app.module.ts
+++ NCDZ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { AvionnageComponent } from './avionnage/avionnage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    AvionnageComponent
   ],
   imports: [
     BrowserModule
