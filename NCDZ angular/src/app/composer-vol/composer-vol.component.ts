49d074a424b3144a462996cd27c5d1d995748689
Xalbat_Itoiz <xalbat.itoiz@gmail.com>
2020-05-26 10:18:42 +0200

    Modif tableau

--- NCDZ angular/src/app/composer-vol/composer-vol.component.ts
+++ NCDZ angular/src/app/composer-vol/composer-vol.component.ts
@@ -4,18 +4,20 @@ import { Parachutiste } from '../parachutiste';
 import { Saut } from '../saut';
 
 @Component({
-  selector: 'composer-vol',
+  selector: 'composer-vol,[composer-vol]',
   templateUrl: './composer-vol.component.html',
   styleUrls: ['./composer-vol.component.css']
 })
 export class ComposerVolComponent implements OnInit {
 
-  avion : Avion = new Avion()
-  avionsDisponibles : Array<Avion> = []
-  listeAttente : Array<Parachutiste> = []
-  listeAttenteGroupe : Array<Array<Parachutiste>> = []
-  listeParaConfirmés : Array<Parachutiste> = []
-  listeSautDemandés : Array<Saut> = []
+  avion : Avion = new Avion();
+  avionsDisponibles : Array<Avion> = [];
+  listeAttente : Array<Parachutiste> = [];
+  listeAttenteGroupe : Array<Array<Parachutiste>> = [];
+  listeParaConfirmés : Array<Parachutiste> = [];
+  listeSautDemandés : Array<Saut> = [];
+  ligne=0;
+  choix = false;
 
   constructor() { }
 
@@ -30,13 +32,18 @@ export class ComposerVolComponent implements OnInit {
   }
 
   loadListeAvionsDispo(){
-    this.avionsDisponibles.push(new Avion(0,2500,4,3,0,60))
-    this.avionsDisponibles.push(new Avion(0,4000,3,3,0,60))
+    this.avionsDisponibles.push(new Avion(15,2500,4,3,0,60))
+    this.avionsDisponibles.push(new Avion(19,4000,3,3,0,60))
   }
 
   loadListesPara(){
     this.listeAttente.push(new Parachutiste(2000,"Dupont","Jean",new Date('2020-10-05')))
     this.listeSautDemandés.push(new Saut(0,2500,true))
+  }
+
+  affichage(id) {
+    this.choix=(!this.choix);
+    this.choix ? this.ligne=id : this.ligne=0;
   }
 
 }
