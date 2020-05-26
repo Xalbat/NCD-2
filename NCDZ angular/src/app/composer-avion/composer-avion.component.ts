fa799dacb9c34075846afacaefb542556d2261e1
Xalbat_Itoiz <xalbat.itoiz@gmail.com>
2020-05-26 14:36:56 +0200

    Modifs

--- NCDZ angular/src/app/composer-avion/composer-avion.component.ts
+++ NCDZ angular/src/app/composer-avion/composer-avion.component.ts
@@ -15,12 +15,14 @@ export class ComposerAvionComponent implements OnInit {
   avion : Avion = new Avion();
   avionsDisponibles : Array<Avion> = [];
   volsDisponibles : Array<Vol> = [];
-  ligneAvion=0;
+  indexAvion=0;
   choixAvion = false;
-  ligneVol=0;
+  indexVol=0;
   choixVol = false;
   listeAttente: Array<Parachutiste> = [];
   listeSautDemandés: Array<Parachutiste> = [];
+  indexRepoSol=0;
+  indexRepoVol=0;
 
   constructor() { }
 
@@ -43,25 +45,58 @@ export class ComposerAvionComponent implements OnInit {
   loadListesVols(){
     this.volsDisponibles.push(new Vol(1, "En cours"))
     this.volsDisponibles.push(new Vol(2, "En attente"))
-    this.volsDisponibles[0].respoSol = new Parachutiste(1,"Robert","Dupont");
+    this.volsDisponibles[0].respoVol = new Parachutiste(1,"Robert","Dupont");
   }
 
   affichageAvion(id) {
     this.choixAvion=(!this.choixAvion);
-    this.choixAvion ? this.ligneAvion=id : this.ligneAvion=0;
+    this.choixAvion ? this.indexAvion=id : this.indexAvion=0;
   }
 
   affichageVol(id) {
     this.choixVol=(!this.choixVol);
-    this.choixVol ? this.ligneVol=id : this.ligneVol=0;
+    this.choixVol ? this.indexVol=id : this.indexVol=0;
   }
 
   loadListesPara(){
-    this.listeAttente.push(new Parachutiste(1,"Dupont","Jean","Instructeur", new Date('2020-10-05')))
+    this.listeAttente.push(new Parachutiste(1,"Instructeur 1","Jean","Instructeur", new Date('2020-10-05')))
     this.listeAttente.push(new Parachutiste(2,"Dupont","Jean","Débutant", new Date('2020-10-05')))
     this.listeAttente.push(new Parachutiste(2,"Dupont","Jean","Débutant", new Date('2020-10-05')))
     this.listeAttente.push(new Parachutiste(2,"Dupont","Jean","Débutant", new Date('2020-10-05')))
-    this.listeAttente.push(new Parachutiste(1,"Dupont","Jean","Instructeur", new Date('2020-10-05')))
-    this.listeAttente.push(new Parachutiste(1,"Dupont","Jean","Instructeur", new Date('2020-10-05')))
+    this.listeAttente.push(new Parachutiste(1,"Instructeur 2","Jean","Instructeur", new Date('2020-10-05')))
+    this.listeAttente.push(new Parachutiste(1,"Instructeur 3","Jean","Instructeur", new Date('2020-10-05')))
   }
+
+  attributionRespoSol() {
+    if (this.choixVol)
+    {
+      if (this.indexRepoSol<0)  
+      {
+        alert(JSON.stringify(this.volsDisponibles[this.indexRepoSol].respoSol))
+        this.volsDisponibles[this.indexRepoSol].respoSol=this.listeAttente[this.indexRepoSol];
+        alert(JSON.stringify(this.volsDisponibles[this.indexRepoSol].respoSol))
+      }
+      else {alert('Choissisez un responsable sol')}
+    }
+    else {alert('Choissisez un vol')}
+  }
+
+  attributionRespoVol() {
+    if (this.choixVol)
+    {
+      if (this.indexRepoVol<0)  
+      {
+        alert(JSON.stringify(this.volsDisponibles[this.indexRepoVol].respoSol))
+        this.volsDisponibles[this.indexRepoVol].respoSol=this.listeAttente[this.indexRepoVol];
+        alert(JSON.stringify(this.volsDisponibles[this.indexRepoVol].respoSol))
+      }
+      else {alert('Choissisez un responsable vol')}
+    }
+    else {alert('Choissisez un vol')}
+  }
+
+  instructeur() {
+    return this.listeAttente.filter(p => p.niveau == 'Instructeur')
+    }
+  
 }
