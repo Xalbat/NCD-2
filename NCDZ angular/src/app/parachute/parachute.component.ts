7f9867d3a36b8b86f70045bf7bb16819b35da618
AntoineVrinat <antoine.vrinat.91@gmail.com>
2020-05-27 16:18:52 +0200

    parachute

--- NCDZ angular/src/app/parachute/parachute.component.ts
+++ NCDZ angular/src/app/parachute/parachute.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Parachute } from '../classes/parachute';
+import { ParachuteService } from '../services/parachute.service';
 
 @Component({
   selector: 'app-parachute',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parachute.component.css']
 })
 export class ParachuteComponent implements OnInit {
+  public formParachute: Parachute = new Parachute();
+  isEditing = false;
 
-  constructor() { }
+  constructor(public srvParachute:ParachuteService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.srvParachute.reload();
   }
 
+  public ajouterParachute() {
+    console.log(this.formParachute)
+    this.srvParachute.add(this.formParachute);
+    this.formParachute = new Parachute();
+  }
+
+  public modifierParachute() {
+    this.isEditing = false;
+    this.srvParachute.update(this.formParachute);
+    this.formParachute = new Parachute();
+  }
+
+  public annulerModification() {
+    this.modifierParachute();
+   }
+ 
+   public editerParachute(parachute) {
+     this.formParachute = JSON.parse(JSON.stringify(parachute));
+     this.isEditing = true;
+   }
+ 
+   public supprimerParachute(Parachute) {
+     this.srvParachute.delete(Parachute);
+   }
 }
--- NCDZ angular/src/app/services/parachute.service.ts
+++ NCDZ angular/src/app/services/parachute.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConfigService } from './app-config.service';
+import { Router } from '@angular/router';
+import { Parachute } from '../classes/parachute';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParachuteService {
+  private apiUrl: string = "";
+  public parachutes: Array<Parachute> = null;
 
-  constructor() { }
+  constructor(private appConfig: AppConfigService, private http: HttpClient, rooter: Router) {
+    this.apiUrl=`${this.appConfig.url}/parachute`;
+   }
+
+
+  public reload() {
+    this.http.get<Array<Parachute>>(this.apiUrl)
+        .subscribe(parachutes => this.parachutes = parachutes);
+  }
+
+  public add(parachute) {
+    this.http.post<Parachute>(this.apiUrl, parachute)
+        .subscribe(respParachute => this.parachutes.push(respParachute));
+  }
+
+  public update(parachute) {
+    this.http.put<Parachute>(`${ this.apiUrl }/${ parachute.id }`, parachute)
+        .subscribe(respParachute => {
+          let index = this.parachutes.indexOf(this.parachutes.find(p => p.id == parachute.id));
+          this.parachutes.splice(index, 1, respParachute);
+        });
+  }
+
+  public delete(parachute) {
+    this.http.delete<Parachute>(`${ this.apiUrl }/${ parachute.id }`)
+        .subscribe(resp => {
+          if (resp) {
+            let index = this.parachutes.indexOf(parachute);
+            this.parachutes.splice(index, 1);
+          }
+        });
+  }
+
 }
+
+
