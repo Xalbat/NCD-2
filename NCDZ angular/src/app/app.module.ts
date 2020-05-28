5a854aaf70fa3b99b93ff482d832e9697986d9d4
Thomas <monsieurgt@gmail.com>
2020-05-28 16:33:37 +0200

    Correction conflit

--- NCDZ angular/src/app/app.module.ts
+++ NCDZ angular/src/app/app.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
     ParachuteComponent,
     AdministrationAvionComponent,
     AdministrationAvionRowComponent,
-    AdministrationAvionFormulaireComponent
+    AdministrationAvionFormulaireComponent,
     ConnectionComponent
   ],
   imports: [
