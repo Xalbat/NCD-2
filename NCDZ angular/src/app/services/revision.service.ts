5e6e714bb2d3e0a94507c069b9911b95bc726617
AntoineVrinat <antoine.vrinat.91@gmail.com>
2020-05-28 15:06:06 +0200

    Revision Service

--- NCDZ angular/src/app/services/revision.service.ts
+++ NCDZ angular/src/app/services/revision.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AppConfigService } from './app-config.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { RevisionParachute } from '../classes/revision-parachute';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RevisionService {
+  private apiUrl: string = "";
+  public revisions: Array<RevisionParachute> = null;
 
-  constructor() { }
+  constructor(private appConfig: AppConfigService, private http: HttpClient, rooter: Router) {
+    this.apiUrl=`${this.appConfig.url}/revision`;
+   }
+
+   public reload() {
+    this.http.get<Array<RevisionParachute>>(this.apiUrl)
+        .subscribe(revisions => this.revisions = revisions);
+  }
+
+  public add(revision) {
+    this.http.post<RevisionParachute>(this.apiUrl, revision)
+        .subscribe(respRevisionParachute => this.revisions.push(respRevisionParachute));
+  }
+
+
 }
